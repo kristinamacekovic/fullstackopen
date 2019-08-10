2eca84c5b6bfd49ac0377c343db119795071b3c0
Kristina Maceković <macekovick@gmail.com>
2019-08-10 16:26:02 +0200

    courseinfo step6

---
+++ part2/part2-1-courseinfo/src/components/Total.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Total = props => {
+  const [part1, part2, part3] = props.parts
+  return (
+    <p>
+      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  )
+}
+
+export { Total }
