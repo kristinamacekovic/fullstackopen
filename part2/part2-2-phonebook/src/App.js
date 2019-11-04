37b6a16be210acbd8f7e6a59bf8dfcd1b06f28d8
Kristina Maceković <macekovick@gmail.com>
2019-11-04 21:46:37 +0100

    Phonebook step6

--- part2/part2-2-phonebook/src/App.js
+++ part2/part2-2-phonebook/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Numbers } from './components/Numbers'
 import { Form } from './components/Form'
 import { Filter } from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [filteredPersons, setFilteredPersons] = useState(persons)
+  const [persons, setPersons] = useState([])
+  const [filteredPersons, setFilteredPersons] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    // console.log('effect fired')
+    axios.get('http://localhost:3001/persons').then(response => {
+      // console.log('promise fulfilled')
+      setPersons(response.data)
+      setFilteredPersons(response.data)
+      // console.log(response.data)
+    })
+  }, [])
 
   const handleNameChange = event => {
     setNewName(event.target.value)
