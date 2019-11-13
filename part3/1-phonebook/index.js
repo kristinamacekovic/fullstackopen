59e3291968c6052d4ddbcc3472ede8a886fabd8b
Kristina Maceković <macekovick@gmail.com>
2019-11-13 18:53:11 +0100

    phonebook backend step 5

--- part3/1-phonebook/index.js
+++ part3/1-phonebook/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+const generateID = () => {
+  return Math.floor(Math.random() * 10000000);
+};
 
 let persons = [
   {
@@ -54,6 +61,24 @@ app.delete("/api/persons/:id", (req, res) => {
   let id = Number(req.params.id);
   persons = persons.filter(person => person.id !== id);
   res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name && !body.number) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateID(),
+  };
+
+  persons.concat(newPerson);
+
+  res.json(newPerson);
 });
 
 const PORT = 3001;
