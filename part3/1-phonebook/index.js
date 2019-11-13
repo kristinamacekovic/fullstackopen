4814f712248a41ca7e982d08fa2c36daa696d260
Kristina Maceković <macekovick@gmail.com>
2019-11-13 18:23:19 +0100

    phonebook backend step4

--- part3/1-phonebook/index.js
+++ part3/1-phonebook/index.js
@@ -5,28 +5,28 @@ let persons = [
   {
     name: "Ada Lovelace",
     number: "39-44-5323523",
-    id: 1
+    id: 1,
   },
   {
     name: "Dan Abramov",
     number: "12-43-234345",
-    id: 2
+    id: 2,
   },
   {
     name: "Mary Poppendieck",
     number: "39-23-6423122",
-    id: 3
+    id: 3,
   },
   {
     name: "Arto Hellas",
     number: "1234",
-    id: 4
+    id: 4,
   },
   {
     name: "Kiko",
     number: "6687596",
-    id: 5
-  }
+    id: 5,
+  },
 ];
 
 app.get("/api/persons", (req, res) => res.json(persons));
@@ -37,7 +37,7 @@ app.get("/api/persons/:id", (req, res) => {
   if (person) {
     res.json(person);
   } else {
-    res.status(404).send();
+    res.status(404).end();
   }
 });
 
@@ -48,6 +48,12 @@ app.get("/info", (req, res) => {
   let message2 = `${d}`;
   res.send(`<div><p>${message}</p>
                 <p>${message2}</p></div>`);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  let id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
