fcc121b674679edf8e92ee7c7f11eed25d1ecec1
Kristina Maceković <macekovick@gmail.com>
2019-12-21 15:22:39 +0100

    part 4

--- part3/1-phonebook/models/phonebook.js
+++ part3/1-phonebook/models/phonebook.js
@@ -1,31 +1,28 @@
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
-mongoose.set('useFindAndDelete', false)
-mongoose.set('useFindAndModify', false)
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+mongoose.set("useFindAndDelete", false);
+mongoose.set("useFindAndModify", false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
 mongoose
-  .connect(
-    url,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(results => console.log('connected'))
-  .catch(err => console.log(err.message))
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(results => console.log("connected"))
+  .catch(err => console.log(err.message));
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, unique: true },
-  number: { type: String, minlength: 8 }
-})
+  number: { type: String, minlength: 8 },
+});
 
-personSchema.plugin(uniqueValidator, { type: 'mongoose-unique-validator' })
+personSchema.plugin(uniqueValidator, { type: "mongoose-unique-validator" });
 
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
